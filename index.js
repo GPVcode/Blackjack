fb8a472a7a169e892da49e6e9dcd1d2b81f40a12
Gabriel Villaruel <gvill005@gmail.com>
2022-08-09 21:47:54 -0700

    first blackjack commit

---
+++ index.js
@@ -0,0 +1,26 @@
+let firstCard = Math.floor(Math.random() * 21) + 1;
+console.log(firstCard)
+let secondCard = Math.floor(Math.random() * 21) + 1;
+console.log(secondCard)
+
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+
+let messageEl = document.getElementById("message-el")
+
+
+function startGame() {
+    if (sum <= 20) {
+        message = "Do you want to draw a new card?"
+    } else if (sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackJack = true
+    } else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageEl.textContent = message;
+}
+
